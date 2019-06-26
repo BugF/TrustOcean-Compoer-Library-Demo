a3730e1e65e08bc99d422d9f07bbcb46a1543d08
Jason long <tanglongjun@qiaokr.com>
2019-06-26 23:07:07 +0800

    fix web page

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -7,7 +7,10 @@ use Londry\TrustOceanSSL\definition\CertificatePeriod;
 use Londry\TrustOceanSSL\definition\CertificateType;
 use Londry\TrustOceanSSL\model\Csr;
 use Londry\TrustOceanSSL\model\Product;
+use Londry\TrustOceanSSL\TrustoceanException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
@@ -16,45 +19,59 @@ class DefaultController extends AbstractController
     /**
      * @Route("/", name="default")
      */
-    public function index()
+    public function index(Request $request, Session $session)
     {
 
-//        $sslOrder = new SslOrder('','');
-//        $sslOrder->setCertificateType(CertificateType::TrustOceanEncryption365Ssl);
-//        $sslOrder->setCertificatePeriod(CertificatePeriod::ThreeMonths);
-//        $sslOrder->setUniqueId('oc5548cn'.time().rand(0,9));
-//        $sslOrder->setDomains([
-//            'test1.trustocean.com','test2.trustocean.com','test3.trustocean.com'
-//        ]);
-//        $sslOrder->setCsrCode(new Csr('-----BEGIN CERTIFICATE REQUEST-----
-//MIIC3jCCAcYCAQAwgZgxCzAJBgNVBAYTAkNOMRIwEAYDVQQIDAlHdWFuZ2Rvbmcx
-//ETAPBgNVBAcMCFNoZW56aGVuMQ0wCwYDVQQKDARhc2RmMQ0wCwYDVQQLDARhc2Rm
-//MR0wGwYDVQQDDBR0ZXN0My50cnVzdG9jZWFuLmNvbTElMCMGCSqGSIb3DQEJARYW
-//dGFuZ2xvbmdqdW5AcWlhb2tyLmNvbTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCC
-//AQoCggEBANE+WUnvpJHztIxgFnPvt+rMPZA2Cd9JDYjq8LOi1acTTgb2DLBRZAdV
-//QxsvCTy/L1Z4fEDhHXK/6jrKYxleDUPBolG9Wtjdw8vjXB/ng5tBzdrF/laTlinD
-//GKRBCPIzLgahY9Twyxag1vneVYj/53UFLnyyLya3oAl/aEmQzILKpa0yxfJ5Cubx
-//ybxQb9hZT+bolIH+ddCXqk8EZ5xkWmnnUTC8jpx6luQBsNzAU5aTH7dEVGvsSj1D
-//vHiyBh7M/MeZTEjZLIN1M1E1qRJz872H7coeu6OheJHDjPI0irhTSxK/jS4r/iU7
-//jubwQQelSHs/BlNtSOSG6yKo43qiyw8CAwEAAaAAMA0GCSqGSIb3DQEBBQUAA4IB
-//AQBCeg4zCZF0sKRWhXWliMP7mB/EnLKlsPoQQayKTjIkOoyrWE9cU2TVZhEdEjqo
-//syEedftaJdYfzXbQTNZcdR6f7lb+Joe1XQfOYa6qvjr0g1gj28ELdSfxCAH9mVdi
-//JpmqGzjZhSIRdFwY5NAYoqzHiD39cxY8zOl0vGH5CN27MiUKSEsrqToY/lb3pni3
-//D6aA0TCfLDmy0jNOMC0iR6VXFOMbGE4eOWfcnnsf9y3B0cvPTqUdI+YizhxOFOaK
-//Tu3c2VuyFtLNsNOuZpj4+0mqoQV1hJTTAMxmCmG6m7KD02JHrc53wWNIb/DREd0D
-//FCsNM8QnJDsZAd1CZZwSFc8q
-//-----END CERTIFICATE REQUEST-----'));
-//        $sslOrder->setDcvMethod([
-//           'dns','dns','dns'
-//        ]);
-//        $sslOrder->setContactEmail('tanglongjun@qiaokr.com');
-//
-//        $newOrder = $sslOrder->callInit(NULL)->callCreate();
-//
-//        var_dump($newOrder->getOrderId());
+        if($request->get('action') === 'apply'){
+            try{
 
+                if($session->get('browserWindowHash') !== $request->get('browserWindowHash')){
+                    throw new TrustoceanException('申请数据中的 Hash 校验失败, 请不要反复刷新您的窗口或使用旧的窗口进行申请. 您可以尝试新开窗口重试。');
+                };
+
+                if(time() - strtotime($session->get('timeToAplly')) < 60*5){
+                    throw new TrustoceanException('每5分钟仅可提交1个新订单, 请您稍后再试');
+                }
+
+                // todo:: you need configurate this API credentials to continue access TrustOcean Reseller API
+                $sslOrder = new SslOrder('yournicename@example.com','APITOKEN-you-can-replace-this-to-your-own-api-token');
+
+                $sslOrder->setCertificateType(CertificateType::TrustOceanEncryption365Ssl);
+                $sslOrder->setCertificatePeriod(CertificatePeriod::ThreeMonths);
+                $sslOrder->setUniqueId('oc5548cn'.time().rand(0,9));
+                $sslOrder->setDomains(explode("\r\n", $request->get('domains')));
+                $sslOrder->setCsrCode(new Csr($request->get('csr_code')));
+
+                $dcvMehods = [];
+                foreach ($sslOrder->getDomains() as $domainName){
+                    $dcvMehods[] = 'dns';
+                }
+                $sslOrder->setDcvMethod($dcvMehods);
+
+                $sslOrder->setContactEmail($request->get('contact_email'));
+
+                $newOrder = $sslOrder->callInit(NULL)->callCreate();
+
+                $session->set('timeToAply', date('Y-m-d H:i:s', time()));
+
+                return $this->render('default/index.html.twig', [
+                    'dcv_info' => $newOrder->getDcvInfo(),
+                    'order_id'  => $newOrder->getOrderId(),
+                    'contact_email' =>  $newOrder->getContactEmail()
+                ]);
+            }catch (TrustoceanException $e){
+                $simpleHash = md5(time().rand(00000,99999));
+                $session->set('browserWindowHash', $simpleHash);
+                return $this->render('default/index.html.twig', [
+                    'error' => $e->getMessage(),
+                    'browserWindowHash' => $simpleHash
+                ]);
+            }
+        }
+        $simpleHash = md5(time().rand(00000,99999));
+        $session->set('browserWindowHash', $simpleHash);
         return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
+            'browserWindowHash' => $simpleHash
         ]);
     }
 }
